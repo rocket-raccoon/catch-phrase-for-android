f06f57448eba9618c12006cef9c3aac214651922
rocket-raccoon <the.alberto.bena@gmail.com>
2014-05-17 20:57:02 -0400

    Added blue / red button drawables

--- src/com/example/catchphrase/endRoundFragment.java
+++ src/com/example/catchphrase/endRoundFragment.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class endRoundFragment extends Fragment {
+	
+	private Button redTeamButton;
+	private Button blueTeamButton;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,23 @@ public class endRoundFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.end_round_fragment, parent, false);
+		
+		redTeamButton = (Button) v.findViewById(R.id.redTeamButton);
+		redTeamButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+						
+			}
+		});
+		
+		blueTeamButton = (Button) v.findViewById(R.id.blueTeamButton);
+		blueTeamButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+						
+			}
+		});
+		
 		return v;
 	}
 
